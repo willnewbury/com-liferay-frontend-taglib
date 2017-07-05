ealonso/liferay-portal

LPS-73319 Implements methods as default

modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ScreenNavigationEntry.java
@@ -36,7 +36,9 @@ public interface ScreenNavigationEntry<T> {
 
 	public String getScreenNavigationKey();
 
-	public boolean isVisible(User user, T formModelBean);
+	public default boolean isVisible(User user, T formModelBean) {
+		return true;
+	}
 
 	public void render(HttpServletRequest request, HttpServletResponse response)
 		throws IOException;

willnewbury/com-liferay-frontend-taglib

frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ScreenNavigationEntry.java
@@ -36,7 +36,9 @@ public interface ScreenNavigationEntry<T> {
 
 	public String getScreenNavigationKey();
 
-	public boolean isVisible(User user, T screenModelBean);
+	public default boolean isVisible(User user, T screenModelBean) {
+		return true;
+	}
 
 	public void render(HttpServletRequest request, HttpServletResponse response)
 		throws IOException;