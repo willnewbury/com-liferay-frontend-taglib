ealonso/liferay-portal

LPS-73319 Screen Navigation Entry interface

modules/apps/foundation/frontend-taglib/frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ScreenNavigationEntry.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib;
+
+import com.liferay.portal.kernel.model.User;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public interface ScreenNavigationEntry<T> {
+
+	public String getCategoryKey();
+
+	public String getEntryKey();
+
+	public String getLabel(Locale locale);
+
+	public String getScreenNavigationKey();
+
+	public boolean isVisible(User user, T formModelBean);
+
+	public void render(HttpServletRequest request, HttpServletResponse response)
+		throws IOException;
+
+}

willnewbury/com-liferay-frontend-taglib

frontend-taglib/src/main/java/com/liferay/frontend/taglib/servlet/taglib/ScreenNavigationEntry.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.taglib.servlet.taglib;
+
+import com.liferay.portal.kernel.model.User;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public interface ScreenNavigationEntry<T> {
+
+	public String getCategoryKey();
+
+	public String getEntryKey();
+
+	public String getLabel(Locale locale);
+
+	public String getScreenNavigationKey();
+
+	public boolean isVisible(User user, T screenModelBean);
+
+	public void render(HttpServletRequest request, HttpServletResponse response)
+		throws IOException;
+
+}